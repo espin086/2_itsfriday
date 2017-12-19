026d0aef537b6ed5a924f51c0e4795f37cc2770c
JJ Espinoza <jj.espinoza@fox.com>
2017-12-19 07:03:20 -0800

    Tasks in ReadMe

--- 1_src/2_python/2_pull_data.py
+++ 1_src/2_python/2_pull_data.py
@@ -1,1 +1,28 @@
+import pandas as pd
+#How to use arguments in a Python Script
+import sys
+if sys.argv[1] == "--h":
+	print ''
+	print '*****************************************************'
+	print '       ************** help menu **************'
+	print '*****************************************************'
+	print ''
+	print 'author: JJ Espinoza'
+	print 'description: data acquring and cleaning'
+	print 'created: 2017-12-13'
+	print ''
+	print 'commands:                 descriptions:'
+	print '__________________________________________________'
+	print ''
+	print '--h                             help menu'
+	print '--TopEndorsers                  identify top endorsers'
+	print '__________________________________________________'
+	print ''
+
+if sys.argv[1] == "--TopEndorsers":
+	print('Top Endorsers:')
+	print '__________________________________________________'
+	df = pd.read_csv('/Users/jje/Documents/00__mytools/2_itsfriday/2_data/1_raw/Endorsements.csv')
+	print(df)
+	
 
---
+++ README.md
@@ -0,0 +1,9 @@
+#To Do List
+
+#Upload my contacts to gmail! - this is a way to recover these contacts after losing my email - write an email to all of them
+
+#Create Email Classifier Tool (want a job, need an employee, your advice) - save classifications in separate csv files, find way to classify new incoming messages, visualize in dashboard
+
+#Personalize email messagages based on company, number of endorsements, etc - ensure to email all people who have given a verbal endorsement. Check messages to ensure I don't spam people. Visualize response rates.
+
+#Visualize top searchers in dashboards
