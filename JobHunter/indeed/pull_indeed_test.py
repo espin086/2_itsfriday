b32bdd2a88da48fe3f7a2f40d2e18cf5b1b1697b
JJ Espinoza <jjespinoza@fb.com>
2020-05-17 09:54:16 -0700

    added automated testing for pull_indeed function

--- JobHunter/indeed/pull_indeed_test.py
+++ JobHunter/indeed/pull_indeed_test.py
@@ -1,12 +1,28 @@
-import pandas as pd
-import pytest
+"""
+Testing that pull_indeed function worked properly
+"""
+
 import pull_indeed
+
+DF = pull_indeed.scrape_indeed('Los Angeles', 'Data Scientist')
 
 
 def test_dataframe_not_empty():
-    df = pd.DataFrame()
-    # df = pull_indeed.scrape_indeed('Los Angeles','Data Scientist')
-    assert df.empty is False, 'dataframe is empty'
+    """
+    Data frame must not be all null or missing values
+    """
+    assert DF.empty is False, 'dataframe is empty'
 
 
-test_dataframe_not_empty()
+def test_each_column_has_data():
+    """
+    Each column in the dataframe must have data
+    """
+    for column in DF.columns:
+        assert DF[column].empty is False,\
+            'column {0} in data is empty'.format(column)
+
+
+if __name__ == "__main__":
+    test_dataframe_not_empty()
+    test_each_column_has_data()
