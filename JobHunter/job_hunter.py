7957066c4785523003c4b440606a52b7e6662653
JJ Espinoza <espin086@gmail.com>
2020-05-17 17:39:24 -0700

    first version of job_hunter

--- JobHunter/job_hunter.py
+++ JobHunter/job_hunter.py
@@ -2,14 +2,21 @@
 Runs the job_hunter program
 """
 
-import pull_indeed
+import pull_indeed as pull_indeed
+import waze
+import tqdm
+
+def job_hunter():
+    CITY = str(input("Search which City, State?:  "))
+    CITY = CITY.replace(" ", "+")
+    JOB_TITLE = str(input("what job title?:  "))
+    JOB_TITLE = JOB_TITLE.replace(" ", "+")
+    TO_ADDRESS = str(input("What is your address?:  "))
+    df = pull_indeed.scrape_indeed(city=CITY, title=JOB_TITLE)
+    return df
 
 
-def menu():
-    string = (
-    """
-    ---------------------------------
-    JOB HUNTER
-    ---------------------------------
-    """)
-    return string
+if __name__ == "__main__":
+    for address in job_hunter()['clean_address']:
+        print(waze.calc_route(TO_ADDRESS=address,                    
+        FROM_ADDRESS='Long Beach, CA'))
--- JobHunter/job_hunter_test.py
+++ JobHunter/job_hunter_test.py
@@ -1,9 +1,10 @@
 import job_hunter
 
 
-def test_menu_print():
+def test_menu_not_empty():
     assert bool(job_hunter.menu()), "Menu is empty"
     return None
+
 
 if __name__ == "__main__":
     test_menu_print()
