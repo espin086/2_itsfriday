88d8b6fd497a7419bc28710da44e7e4a56cc508b
JJ Espinoza <jjespinoza@fb.com>
2020-07-21 18:05:14 -0700

    made job_hunter.py export list of jobs as csvs

--- JobHunter/job_hunter.py
+++ JobHunter/job_hunter.py
@@ -1,22 +1,32 @@
 """
 Runs the job_hunter program
 """
-
+import pandas as pd
 import pull_indeed as pull_indeed
 import waze
-import tqdm
+from tqdm import tqdm
 
 def job_hunter():
     CITY = str(input("Search which City, State?:  "))
     CITY = CITY.replace(" ", "+")
     JOB_TITLE = str(input("what job title?:  "))
     JOB_TITLE = JOB_TITLE.replace(" ", "+")
-    TO_ADDRESS = str(input("What is your address?:  "))
+
     df = pull_indeed.scrape_indeed(city=CITY, title=JOB_TITLE)
     return df
 
 
 if __name__ == "__main__":
-    for address in job_hunter()['clean_address']:
-        print(waze.calc_route(TO_ADDRESS=TO_ADDRESS,
+    TO_ADDRESS = str(input("What is your address?:  "))
+    JOB_HUNTER = job_hunter()
+    print(JOB_HUNTER)
+    COMMUTES = []
+
+    for address in tqdm(JOB_HUNTER['clean_address']):
+
+        COMMUTES.append(waze.calc_route(TO_ADDRESS=TO_ADDRESS,
         FROM_ADDRESS=address))
+
+    commutes = pd.DataFrame(COMMUTES, columns=['waze'])
+    results = pd.concat([JOB_HUNTER, commutes], axis=1, sort=False)
+    results.to_csv("results/results.csv", encoding='utf-8')
